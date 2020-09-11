027080db0072979c83b056b78d04c495b9c6e57b
Mark Rogers <mrogers@sabreindustries.com>
2020-09-11 17:35:06 +0000

    Update to PLS wait time from 10 sec to 5 sec;  Removed labels from generated PLS DXF
    git-tfs-id: [https://iasc-dev-tfs1/Sabre]$/Business Processes/Engineering/2019_base/PLS Post Processor;C22419

--- PLS Post Processor/Run.cs
+++ PLS Post Processor/Run.cs
@@ -1,17 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 using System.Xml;
 using System.Diagnostics;
 using System.IO.Compression;
 
+using WW.Cad.Model;
+using WW.Cad.IO;
+using System.Drawing;
+using WW.Cad.Drawing;
+using WW.Math;
+
+using NLog;
+using WW.Cad.Model.Entities;
+
+//using SixLabors.ImageSharp.PixelFormats;
+//using SixLabors.ImageSharp;
+
 namespace PLS_Post_Processor
 {
+    /// <summary>
+    /// Define the DXF export to image format
+    /// </summary>
+    enum ImageExportFormat
+    {
+        Unknown,
+        Png,
+        Jpg,
+        Bmp,
+        Gif
+    }
+
     public class Run
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        private static string plsDxfFullPath = "";
+
+        private const int maxPlsWaitTimeMilli = 5000;  // ==> milliseconds
+
         public static void RunApp()
         {
             string polPath = ParsePolPath();
@@ -26,24 +52,32 @@ namespace PLS_Post_Processor
             }
 
             File.Copy(polPath, tempPolPath, true);
+
+            string lcaPath = Path.ChangeExtension(polPath, "lca");
+            string tempLcaPath = Path.ChangeExtension(tempPolPath, "lca");
+            File.Copy(lcaPath, tempLcaPath, true);
+
             CreateCommandFile(tempPolPath, stagingPath);
             RunCommandFile();
+
+            DxfImageExporter();
+
             File.Delete(tempPolPath);
+            File.Delete(tempLcaPath);
             ZipFile.CreateFromDirectory(stagingPath, uploadFile);
         }
 
         private static string ParsePolPath()
         {
             string postprocPath = @"c:\pls\temp\postproc.xml";
-            
+
             XmlDocument xdoc = new XmlDocument();
             xdoc.Load(postprocPath);
-            
+
             XmlNodeList nl = xdoc.GetElementsByTagName("creator");
             string polPath = nl[0].Attributes["projectpath"].Value;
 
             return polPath;
-            
         }
 
         private static void CreateCommandFile(string tempPolPath, string stagingPath)
@@ -60,14 +94,24 @@ namespace PLS_Post_Processor
                     File.Delete(f);
                 }
             }
-            
+
             string cmdFilePath = @"c:\pls\temp\postcmd.cmd";
+            plsDxfFullPath = $@"{stagingPath}\pe.dxf";
+
+            // *** PLS API parameters for DXF.
+            string code3D = "1";            // 1 = 3D, 0 = 2D
+            string drawMode = "2";          // 0 = line, 1 = wireframe, 2 = rendered
+            string displayLabels = "0";     // 1 = on, 0 = off
+            string longitude = "20";        // longitude of view in degrees
+            string latitude = "30";         // latitude of view in degrees
 
             using(StreamWriter sw = new StreamWriter(cmdFilePath, false))
             {
                 sw.WriteLine(@"TYPE='PLS POSTPROC COMMAND FILE' VERSION='1' UNITS='US' SOURCE='PLS-POLE' USER='Sabre Industries, Inc.' FILENAME='c:\pls\temp\postcmd.cmd'");
                 sw.WriteLine($@"49 '{tempPolPath}'");
-                sw.WriteLine($@"42 '{stagingPath}\pe.dxf' 1 2 2 60 30");
+                sw.WriteLine($@"42 '{plsDxfFullPath}' {code3D} {drawMode} {displayLabels} {longitude} {latitude}");
+                //sw.WriteLine($@"42 '{plsDxfFullPath}' 1 2 1 60 30");
+                //sw.WriteLine($@"42 '{stagingPath}\pe.dxf' 1 2 2 60 30");
                 sw.WriteLine("3 ; exit");
             }
 
@@ -84,7 +128,114 @@ namespace PLS_Post_Processor
 
                 var proc = new Process { StartInfo = psi };
                 proc.Start();
-                proc.WaitForExit(10000);
+                proc.WaitForExit(maxPlsWaitTimeMilli); // 10000);
+        }
+
+        /// <summary>
+        /// Read the DXF drawing and export to the specified image format (default = png)
+        /// </summary>
+        /// <remarks>
+        /// See Wout's URL: http://www.woutware.com/doc/cadlib4.0/api/WW.Cad.IO.DxfReader.html
+        /// </remarks>
+        private static void DxfImageExporter()
+        {
+            ImageExportFormat format = ImageExportFormat.Png;
+            if (format == ImageExportFormat.Unknown)
+            {
+                Logger.Error("Unknown image format.");
+                return;
+            }
+
+            string dxfFilePath = plsDxfFullPath;
+            string sFormat = format.ToString().ToLower();
+
+            DxfModel model = null;
+            try {
+                model = CadReader.Read(dxfFilePath);
+            } catch (Exception e) {
+                string errMsg = $"Error opening DXF during DXF image export.\r\n{dxfFilePath}";
+                Logger.Error(e, errMsg);
+            }
+
+            CleanupDxfDrawing(model); //, dxfFilePath);
+
+            // *** The image output file full path with defined extension.
+            string outfile = Path.ChangeExtension(dxfFilePath, sFormat);
+
+            Stream stream;
+            Size maxSize = new Size(500, 500);
+            //Image<Bgra32> bitmap = ImageExporter.CreateAutoSizedBitmap<Bgra32>(
+            Bitmap bitmap =
+                ImageExporter.CreateAutoSizedBitmap(model, Matrix4D.Identity,
+                    GraphicsConfig.AcadLikeWithWhiteBackground, maxSize);
+                    //GraphicsConfig.AcadLikeWithBlackBackground, maxSize);
+
+            switch (format) {
+                case ImageExportFormat.Bmp:
+                    using (stream = File.Create(outfile))
+                    {
+                        ImageExporter.EncodeImageToBmp(bitmap, stream);
+                    }
+                    break;
+                case ImageExportFormat.Gif:
+                    using (stream = File.Create(outfile))
+                    {
+                        ImageExporter.EncodeImageToGif(bitmap, stream);
+                    }
+                    break;
+                case ImageExportFormat.Png:
+                    using (stream = File.Create(outfile))
+                    {
+                        ImageExporter.EncodeImageToPng(bitmap, stream);
+                    }
+                    break;
+                case ImageExportFormat.Jpg:
+                    using (stream = File.Create(outfile))
+                    {
+                        ImageExporter.EncodeImageToJpeg(bitmap, stream);
+                    }
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Clean up the DXF model by removing Annotation layer (if present) and
+        /// make the text smaller (9/8/20 -- not working yet!!)
+        /// </summary>
+        /// <param name="model"></param>
+        private static void CleanupDxfDrawing(DxfModel model) //, string dxfFullPath)
+        {
+            // *** Turn off the Annotation layer if it exists.
+            string annoLayer = "Annotation";
+            if (model.Layers.Keys.Contains(annoLayer))
+            {
+                model.Layers[annoLayer].Enabled = false;
+            }
+
+            //BoundsCalculator boundsCalculator = new BoundsCalculator();
+
+            foreach (var entity in model.Entities)
+            {
+                string eType = entity.EntityType;
+                if (!eType.Equals("TEXT", StringComparison.OrdinalIgnoreCase))
+                {
+                    continue;
+                }
+
+                DxfText txtEntity = entity as DxfText;
+                double txtHt = txtEntity.Height / 2.0;
+                string txt = txtEntity.Text;
+                var txtPt = txtEntity.AlignmentPoint1;
+
+                //boundsCalculator.GetBounds(model, txtEntity);
+                //Bounds3D bounds = boundsCalculator.Bounds;
+
+                DxfText newTxt = new DxfText(txt, txtPt, txtHt);
+                txtEntity = newTxt;
+            }
+
+            string dxfFullPath = model.Filename;
+            DxfWriter.Write(dxfFullPath, model);
         }
     }
 }
