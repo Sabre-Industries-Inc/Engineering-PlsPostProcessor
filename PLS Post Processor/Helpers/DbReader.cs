ebac92766e59fc7aaae09e234ce24a9ffca4415e
Mark Rogers <mrogers@sabreindustries.com>
2024-04-18 22:48:21 +0000

    add check the SFC database for the PLS effective date to make sure the correct version is being used.
    git-tfs-id: [https://iasc-dev-tfs1/Sabre]$/Business Processes/Engineering/2019_base/PLS Post Processor;C33661

---
+++ PLS Post Processor/Helpers/DbReader.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLS_Post_Processor.Helpers
+{
+    internal class DbReader
+    {
+        public static (decimal supportedVersion, bool isSupported) GetSupportedPlsVersion(decimal plsVersion)
+        {
+            string fieldName = "SupportedPLSVersion";
+            decimal supportedVersion = 0;
+            bool plsVersionIsSupported = false;
+            try
+            {
+                using (SqlConnection conn = EnvironmentHelper.GetQuoteSqlConnection())
+                {
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cmd.CommandText = buildSql();
+                    cmd.Connection = conn;
+                    conn.Open();
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        supportedVersion = (decimal)reader[fieldName];
+                    }
+                    // *** Get result from second query.
+                    reader.NextResult();
+                    while (reader.Read())
+                    {
+                        int isSupported = (int)reader["isSupportedVersion"];
+                        plsVersionIsSupported = (isSupported == 1);
+                    }
+                }
+
+                return (supportedVersion, plsVersionIsSupported);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error executing DbReader.GetQuoteList--{ex.Message}", ex);
+            }
+
+            string buildSql()
+            {
+                string sql = $@"
+                    SELECT TOP 1 SupportedPLSVersion
+                      FROM PLSProperties
+                      WHERE CONVERT(DATE, GETDATE()) >= CONVERT(DATE, EffectiveStartDate)
+	                    AND (CONVERT(DATE, GETDATE()) < CONVERT(DATE, EffectiveEndDate)
+	                    OR ISNULL(EffectiveEndDate, 0) = 0)
+	                    ORDER BY SupportedPLSVersion
+
+                    SELECT
+	                    CASE
+		                    WHEN CONVERT(DATE, GETDATE()) >= CONVERT(DATE, EffectiveStartDate)
+			                    AND (CONVERT(DATE, GETDATE()) < CONVERT(DATE, EffectiveEndDate) OR EffectiveEndDate IS NULL)
+			                    THEN 1
+                        ELSE 0
+	                    END AS isSupportedVersion
+	                    FROM PLSProperties
+	                    WHERE SupportedPLSVersion = {plsVersion:f2};
+                ";
+
+                return sql;
+            }
+        }
+    }
+}
---
+++ PLS Post Processor/Helpers/EnvironmentHelper.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLS_Post_Processor.Helpers
+{
+    public enum MyEnvironment
+    {
+        dev,
+        test,
+        prod
+    }
+
+    internal class EnvironmentHelper
+    {
+        public static MyEnvironment GetEnvironment()
+        {
+#if DEBUG
+            return MyEnvironment.dev;
+#else
+            string assemblyDirectory = GetExePath();
+            if (assemblyDirectory.IndexOf("dev", StringComparison.OrdinalIgnoreCase) > 0)
+            {
+                return MyEnvironment.dev;
+            }
+            else if (assemblyDirectory.IndexOf("test", StringComparison.OrdinalIgnoreCase) > 0)
+            {
+                return MyEnvironment.test;
+            }
+            else
+            {
+                return MyEnvironment.prod;
+            }
+#endif
+        }
+
+        private static string GetExePath()
+        {
+            string assemblyPath = Assembly.GetExecutingAssembly().Location;
+            string assemblyDirectory = Path.GetDirectoryName(assemblyPath);
+
+            return assemblyDirectory;
+        }
+
+        public static SqlConnection GetQuoteSqlConnection()
+        {
+            SqlConnection conn = new SqlConnection(GetQuoteConnectionString());
+            return conn;
+        }
+
+
+        internal static string GetQuoteConnectionString()
+        {
+            var environ = GetEnvironment();
+
+            switch (environ)
+            {
+                case MyEnvironment.dev:
+                    return @"Data Source=txke-sql-dev;Initial Catalog=Quote;Trusted_Connection=True;";
+
+                case MyEnvironment.test:
+                    return @"Data Source=txke-sql-test;Initial Catalog=Quote;Trusted_Connection=True;";
+
+                case MyEnvironment.prod:
+                    return @"Data Source=txke-sql-sueet;Initial Catalog=Quote;Trusted_Connection=True;";
+
+                default:
+                    return string.Empty;
+            }
+        }
+    }
+}
